c01909a2978c7185f2068a4fcf5740f687b63b44
martin835 <64438132+martin835@users.noreply.github.com>
2022-04-08 16:04:35 +0200

    album artist loading data from redux

--- spotify-react-refactor/src/components/BodyAlbum.jsx
+++ spotify-react-refactor/src/components/BodyAlbum.jsx
@@ -27,80 +27,85 @@ const BodyAlbum = (props) => {
     props.loadArtistAlbum(params.albumId);
   }, []);
   console.log(params.albumId);
-  console.log(props.artistAlbum.tracks.data);
+
   return (
     <div>
-      <div className="top-bumper"></div>
-      <Container fluid>
-        <Row>
-          <Col xs={12} className="card-head d-flex my-2">
-            <Col xs={2} className="card-head-image">
-              <img
-                className="card-image-head"
-                src={require("../Assests/album_pic.jpeg")}
-                alt=""
-              />
-            </Col>
-            <Col
-              xs={8}
-              className="d-flex description text-white justify-content-start align-items-end"
-            >
-              <div>
-                <p className=" description-head_1 mb-0">
-                  {" "}
-                  {props.artistAlbum.record_type}{" "}
-                </p>
-                <h3 className=" description-head_2 mb-0">
-                  {" "}
-                  {props.artistAlbum.title}{" "}
-                </h3>
-                <p className=" text-muted description-head_3">
-                  {" "}
-                  <strong>{props.artistAlbum.artist.name}</strong> .{" "}
-                  {props.artistAlbum.release_date} .{" "}
-                  {props.artistAlbum.tracks.data.length} songs,{" "}
-                  {props.artistAlbum.duration}
-                </p>
-              </div>
-            </Col>
-          </Col>
-          <div>
-            <Row className="text-white">
-              <Col xs={12} className="d-flex align-items-center">
-                <div className="artist-main-play-button">
-                  <i className="bi bi-play-circle d-inline-block mx-2"></i>
-                  <div className="artist-main-button-inner-div"></div>
-                </div>
-                <div className="ml-4 px-3 py-1 artist-follow">
-                  {" "}
-                  <i className="bi bi-heart"></i>
-                </div>
-                <div className="ml-4">
-                  <i className="bi bi-three-dots"></i>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Row>
-      </Container>
-      <div className="pt-2">
-        <span className="pl-3 ash_tag">#</span>
-        <span className="mx-2" style={{ color: "white" }}>
+      {props.artistAlbum.artist.name && (
+        <>
           {" "}
-          TITLE
-        </span>
-      </div>
-      <hr className="my-2 hr-album" />
-      <div className="artist-songs-table-wrapper pr-3">
-        {props.artistAlbum.tracks.data.map((song, i) => (
-          <PlaylistItem
-            song={song}
-            i={i}
-            smallImg={props.artistAlbum.cover_small}
-          />
-        ))}
-      </div>
-      <div className="bottom-bumper"></div>;
+          <div className="top-bumper"></div>
+          <Container fluid>
+            <Row>
+              <Col xs={12} className="card-head d-flex my-2">
+                <Col xs={2} className="card-head-image">
+                  <img
+                    className="card-image-head"
+                    src={props.artistAlbum.cover_medium}
+                    alt=""
+                  />
+                </Col>
+                <Col
+                  xs={8}
+                  className="d-flex description text-white justify-content-start align-items-end"
+                >
+                  <div>
+                    <p className=" description-head_1 mb-0">
+                      {" "}
+                      {props.artistAlbum.record_type}{" "}
+                    </p>
+                    <h3 className=" description-head_2 mb-0">
+                      {" "}
+                      {props.artistAlbum.title}{" "}
+                    </h3>
+                    <p className=" text-muted description-head_3">
+                      {" "}
+                      <strong>{props.artistAlbum.artist.name}</strong> .{" "}
+                      {props.artistAlbum.release_date} .{" "}
+                      {props.artistAlbum.tracks.data.length} songs,{" "}
+                      {props.artistAlbum.duration}
+                    </p>
+                  </div>
+                </Col>
+              </Col>
+              <div>
+                <Row className="text-white">
+                  <Col xs={12} className="d-flex align-items-center">
+                    <div className="artist-main-play-button">
+                      <i className="bi bi-play-circle d-inline-block mx-2"></i>
+                      <div className="artist-main-button-inner-div"></div>
+                    </div>
+                    <div className="ml-4 px-3 py-1 artist-follow">
+                      {" "}
+                      <i className="bi bi-heart"></i>
+                    </div>
+                    <div className="ml-4">
+                      <i className="bi bi-three-dots"></i>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Row>
+          </Container>
+          <div className="pt-2">
+            <span className="pl-3 ash_tag">#</span>
+            <span className="mx-2" style={{ color: "white" }}>
+              {" "}
+              TITLE
+            </span>
+          </div>
+          <hr className="my-2 hr-album" />
+          <div className="artist-songs-table-wrapper pr-3">
+            {props.artistAlbum.tracks.data.map((song, i) => (
+              <PlaylistItem
+                song={song}
+                i={i}
+                smallImg={props.artistAlbum.cover_small}
+              />
+            ))}
+          </div>
+          <div className="bottom-bumper"></div>
+        </>
+      )}
     </div>
   );
 };
